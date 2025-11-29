3128c3237b7b5f11b6af94ce7e5d3d75ade185f3
javakishore-veleti <javakishore@gmail.com>
2025-11-29 18:08:20 -0500

    Added github actions name: GCP Deploy Latest to Cloud Run

--- service/main.py
+++ service/main.py
@@ -59,7 +59,10 @@ def upload_parquet():
     return response
 
 
-app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5052)))
+# app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5052)))
+# print(__name__)
 
-print(__name__)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
