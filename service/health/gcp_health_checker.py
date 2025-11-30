58fb2c2001600fab8d9f945e6cc1e4b11d733c10
javakishore-veleti <javakishore@gmail.com>
2025-11-29 22:32:13 -0500

    Implemented healthz

--- service/health/gcp_health_checker.py
+++ service/health/gcp_health_checker.py
@@ -8,25 +8,39 @@ LOGGER = logging.getLogger(__name__)
 
 class GCPHealthChecker(HealthChecker):
     def __init__(self):
+        LOGGER.info("Initializing GCP Health Checker STARTED")
+
         self.bigquery_client = bigquery.Client()
         self.storage_client = storage.Client()
         HealthFacade.get_instance().register(self)  # Auto-register
+
+        LOGGER.info("Initializing GCP Health Checker COMPLETE")
 
     def name(self):
         return "GCP"
 
     def liveness(self):
+        LOGGER.info("GCPHealthChecker.liveness Entered")
+
         try:
             _ = bigquery.Client()
             _ = storage.Client()
+
+            LOGGER.info("GCPHealthChecker.liveness Exiting True")
             return True
         except Exception:
+            LOGGER.info("GCPHealthChecker.liveness Exiting False")
             return False
 
     def readiness(self):
+        LOGGER.info("GCPHealthChecker.readiness Entered")
+
         try:
             list(self.bigquery_client.list_datasets())
             list(self.storage_client.list_buckets())
+
+            LOGGER.info("GCPHealthChecker.readiness Exiting True")
             return True
         except Exception:
+            LOGGER.info("GCPHealthChecker.readiness Exiting False")
             return False
