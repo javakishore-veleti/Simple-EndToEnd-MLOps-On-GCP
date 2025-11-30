7c4abd36f0dbb6923f3529d9a01c80f1fb4d7312
javakishore-veleti <javakishore@gmail.com>
2025-11-29 21:58:51 -0500

    Added github actions name: GCP Deploy Latest to Cloud Run

--- service/dao/gcp_facade.py
+++ service/dao/gcp_facade.py
@@ -1,7 +1,9 @@
 
 from google.cloud import bigquery
 from google.cloud import storage
+import logging
 
+LOGGER = logging.getLogger(__name__)
 
 class GcpFacade:
     def __init__(self):
@@ -9,17 +11,31 @@ class GcpFacade:
         pass
 
     def get_or_create_client(self) -> bigquery.Client:
+        LOGGER.info('get_or_create_client Entered')
         """Return a BigQuery client using Cloud Run's service account."""
-        return bigquery.Client()
+
+        client_obj = bigquery.Client()
+
+        LOGGER.info('get_or_create_client Exiting client_obj')
+        return client_obj
 
     def get_storage_client(self) -> storage.Client:
+        LOGGER.info('get_storage_client Entered')
+
         """Return a GCS client using Cloud Run's service account."""
-        return storage.Client()
+        client_obj = storage.Client()
+
+        LOGGER.info('get_storage_client Exiting client_obj')
+        return client_obj
 
     def download_blob(self, bucket_name: str, source_blob_name: str, destination_file_name: str):
+        LOGGER.info('download_blob Entered')
+
         """Download a blob from GCS to a local file."""
         storage_client = self.get_storage_client()
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(source_blob_name)
         blob.download_to_filename(destination_file_name)
         print(f"Downloaded {source_blob_name} from bucket {bucket_name} to {destination_file_name}")
+
+        LOGGER.info('download_blob Exiting')
